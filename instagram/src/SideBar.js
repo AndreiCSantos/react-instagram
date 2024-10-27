de6eb31e53f8cb9e4f31c5f42a402c5a13e0f3ab
AndreiCS <andreicsantos98@hotmail.com>
2024-10-27 19:26:46 -0300

    Adicionar componente SideBar com integração dos componentes User e Suggestions

---
+++ instagram/src/SideBar.js
@@ -0,0 +1,12 @@
+import User from "./User";
+import Suggestions from "./Suggestions";
+
+export default function SideBar() {
+
+    return (
+        <div className="sidebar">
+            <User />
+            <Suggestions />
+        </div>
+    )
+}
